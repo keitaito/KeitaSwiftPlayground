5b56af9276af241a37f9e9514cdafb3b31b99a2a
Keita Ito <keitaitok@gmail.com>
2015-12-03 23:57:03 -0800

    Initial commit

---
+++ KeitaSwiftPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let description = "Swift is open source now, Dec 3rd 2015!"
+
+let myIntArray = [1, 2, 3, 4, 5]
+let mappedArray = myIntArray.map { x in x * 2 }
+mappedArray
+
